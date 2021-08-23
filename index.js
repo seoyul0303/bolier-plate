13df1a553e1d53e98a2f91379a2d0642661c9107
seoyuri <seoyul0303@gmail.com>
2021-08-23 16:55:54 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://seoyul:yr10791079@bolier-plate.sifb5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!~~~~안녕하세요')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
