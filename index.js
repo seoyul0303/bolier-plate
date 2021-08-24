5174502d69147c77d32e817174bd770facf196d4
seoyuri <seoyul0303@gmail.com>
2021-08-24 13:17:37 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require('./models/User');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://seoyul:yr10791079@bolier-plate.sifb5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+//application/x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded());
+
+//application/json
+//app.use(bodyParser.json());
+app.use(express.json());
+
+const mongoose = require('mongoose') 
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~~~~안녕하세요')
+  res.send('Hello World!~~~~안녕하세요~!!!!')
+})
+
+app.post('/register', (req, res) => {
+  //회원 가입할 때 필요한 정보를 client 에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user =  new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })//몽고db
+
+
+
 })
 
 app.listen(port, () => {
